e490e759883550b8af9cfa9f071c0332d6cdc974
Elliot Bendoly <bendoly.2@osu.edu>
2019-06-23 12:05:22 -0400

    Add files via upload
    Files associated with the example AboutMe lab activity in the Swift curriculum

---
+++ AboutMe.v3/AboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Elliot Bendoly on 6/21/19.
+//  Copyright © 2019 Elliot Bendoly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
